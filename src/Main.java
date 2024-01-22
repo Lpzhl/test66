5a5ddf6665beb13b7a4ed0c811afc4f533d5cf8d
lzh <2212384795@qq.com>
2024-01-22 15:28:59 +0800

    登录

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("廖梓行");
+    }
+
+    public void login(){
+
+        System.out.println("用户登录");
     }
 }
