c88e5ba1f320b147db01ec1493289e1267557760
lzh <2212384795@qq.com>
2024-01-22 15:45:22 +0800

    master分支

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 public class Main {
+
+    /**
+     * master分支
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //fdf
@@ -9,5 +14,9 @@ public class Main {
         //777
 
         //66666
+    }
+    public void register(){
+
+        System.out.println("用户注册");
     }
 }
